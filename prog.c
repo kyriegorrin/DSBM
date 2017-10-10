b6716a6fc8c8dd67f727d59e096b14f929703cbe
Ricardo Zarco Badia <ricard.zarco@c5s202pc105.fib.upc.es>
2017-10-10 16:34:06 +0200

    Segona versio de get time

--- prog.c
+++ prog.c
@@ -1,4 +1,4 @@
-#include <time.h>
+#include <sys/time.h>
 #include <stdio.h>
 #include <wiringPi.h>
 
@@ -26,24 +26,36 @@ void discharge(){
     sleep(1);
 }
 
+long long timeInMilliseconds(){
+	struct timeval tv;
+
+	gettimeofday(&tv, NULL);
+	return (((long long)tv.tv_sec)*1000)+(tv.tv_usec/1000);
+}
+
 int main(){    
+
+initializeGPIO();
 //Memes
     while(1){
         //Inicialitzem clocks i posem high
-        t1 = clock();
+        //t1 = clock();
+	long long t1 = timeInMilliseconds();
         digitalWrite(outPin, HIGH);
         
         //Esperem fins a detecció de high
         while(digitalRead(inPin) != HIGH);
         
         //Agafem temps i fem diferencia
-        t2 = clock();
-        float diff = ((float)(t2 - t1) / 1000000.0F ) * 1000;
+        //t2 = clock();
+	long long t2 = timeInMilliseconds();
+        //float diff = ((float)(t2 - t1) / 1000000.0F ) * 1000;
+	long long diff = t2 - t1;
         
         //Descarreguem condensador
         discharge();
         
         //Printa temps (si no printa o shenaniggas, comenta-ho))
-        printf("Temps: %f\n", diff);
+        printf("Temps: %lld\n", diff);
     }
 }
